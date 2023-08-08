e3a219ff5d88b5e5aabda758b3e7c6bdbdb5f1ba
EsmaeeilMoradi <esmaeeilmoradi700@gmail.com>
2023-08-08 20:21:23 +0330

    Add : EventLogger.java , LocalEventLogger.java .

--- app/src/main/java/com/esm/settingsintelligencesample/MainActivity.java
+++ app/src/main/java/com/esm/settingsintelligencesample/MainActivity.java
@@ -3,12 +3,33 @@ package com.esm.settingsintelligencesample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.save_btn);
+        EditText editText = findViewById(R.id.text);
+        // Re-created activities receive the same MyViewModel instance created by the first activity.
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
     }
 }
